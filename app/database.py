943327e623967f25296d87942d67d1c95e4c14bb
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2025-04-24 18:44:34 +0300

    set up database connection

---
+++ app/database.py
@@ -0,0 +1,17 @@
+# app/database.py
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLite URL
+SQLALCHEMY_DATABASE_URL = "sqlite:///./health.db"
+
+# Create engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+# Session for DB access
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base class for models
+Base = declarative_base()
