60a27e2a95cd449322ff07a1a4b9cf38cec16ecb
sittuk-kibet <kibetnathan@kabarak.ac.ke>
2025-04-24 21:46:11 +0300

    set up entry point

---
+++ app/main.py
@@ -0,0 +1,16 @@
+# app/main.py
+
+from fastapi import FastAPI
+from app.database import engine
+from app.models import models
+from app.routers import clients, programs
+
+# Create all tables
+models.Base.metadata.create_all(bind=engine)
+
+# Initialize app
+app = FastAPI(title="Health Info System")
+
+# Register routers
+app.include_router(clients.router)
+app.include_router(programs.router)
